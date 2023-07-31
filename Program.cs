d89f60f497801980515cbdd8a7add86cf75e5336
User <User@WS-SENDERUK>
2023-07-31 10:46:27 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_7
+{
+    class Program
+    {
+        static void Main()
+        {
+            string stringNumber = "256";
+
+            int numberFromString = Convert.ToInt32(stringNumber); ;
+
+            int square = numberFromString * numberFromString;
+
+            Console.WriteLine("Квадрат введенного числа равен " + square);
+        }
+    }
+}
